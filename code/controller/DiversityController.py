390e44d8f21be3e578553a90398be7fc6568c43b
Saurabh Nigam <saurabh.nigam@impetus.co.in>
2021-05-29 23:19:39 +0530

    Added the View Controller layer

---
+++ code/controller/DiversityController.py
@@ -0,0 +1,135 @@
+#Dash Processing & Analysis
+#@100rabh.nigam
+#Team Basketball
+
+#This program is VC layer using Dash components & cleansed data
+
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+from pandas_datareader import data as web
+from datetime import datetime as dt
+
+#Constants
+RACE_WHITE =1 
+RACE_BLACK =2
+RACE_ASIAN =3
+GENDER_MALE =1
+GENDER_FEMALE=2
+
+#Preparing UI
+app = dash.Dash('How diverse is your position?')
+df = pd.read_csv("/home/impadmin/Saurabh/Projects/BasketBall/Corey/cpsaat11_2.csv")
+
+app.layout = html.Div([
+    #Take user occupation
+    dcc.Dropdown(id='job-dropdown', options=[
+        {'label': row['Occupation'], 'value': row['SN']} for index, row in df.iterrows()
+    ], placeholder='Your Occupation'),    
+    #User's gender
+    dcc.Dropdown(id='gender-dropdown', options=[
+        {'label': 'Male', 'value': GENDER_MALE},
+        {'label': 'Female', 'value': GENDER_FEMALE} 
+    ], placeholder='Gender'), 
+    #User's race
+    dcc.Dropdown(id='race-dropdown', options=[
+        {'label': 'White', 'value': RACE_WHITE},
+        {'label': 'Black or African American', 'value': RACE_BLACK},
+        {'label': 'Asian', 'value': RACE_ASIAN}
+    ], placeholder='Your Race'),
+    #Submit Action button
+    html.Button(id='submit-button', n_clicks=0, children='Submit'),
+    html.Div(id='output-state'),
+    #Graph
+    dcc.Graph(id='my-graph'),
+
+    #Header
+    html.H4(children='US Occupations'),
+    #Reporting Dropdown
+    dcc.Dropdown(id='dropdown', options=[
+        {'label': row['Occupation'], 'value': row['SN']} for index, row in df.iterrows()
+    ], multi=True, placeholder='Filter by Occupation'),
+    #Reporting Table view
+    html.Div(id='table-container')
+
+], style={'width': '500'})
+
+
+def generate_table(dataframe, max_rows=10):
+    """
+    Draws a table based on df
+    """
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+@app.callback(Output('submit-button', 'disabled'),
+              Input('submit-button-state', 'n_clicks'),
+              Input('job-dropdown', 'value'),
+              Input('gender-dropdown', 'value'),
+              Input('race-dropdown', 'value'))
+def update_output(n_clicks, job_dropdown_value, gender_dropdown_value,race_dropdown_value):
+    """
+    Callback for handling submit action
+    """
+    # add this record to df 
+    #propogate data to parent
+    #Store csv
+    print ('''
+        Adding record ,
+        job_dropdown_value  is "{}",
+        ,gender_dropdown_value is "{}"
+        and race_dropdown_value is "{}"
+    ''').format( job_dropdown_value, gender_dropdown_value, race_dropdown_value)
+    return True
+
+
+@app.callback(
+    Output('table-container', 'children'),
+    [Input('dropdown', 'value')])
+def display_table(dropdown_value):
+    """
+    Callback to display filtered table
+    """
+    if dropdown_value is None:
+        return generate_table(df, 1000)
+    
+    dff = df.where(df.SN.isin(dropdown_value))
+    return generate_table(dff,1000)
+
+
+@app.callback(Output('my-graph', 'figure'), [Input('dropdown', 'value')])
+def update_graph(dropdown_value):
+    """
+    Callback to update graph
+    """
+    #Devise mechanism to grade based on 
+    #Grades back to df
+    if dropdown_value is None:
+        dff=df
+    else:
+        dff=df.where(df.SN.isin(dropdown_value))
+
+    return {
+        'data': [{
+            'x': dff.Occupation,
+            'y': dff.Women
+        }],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+
+if __name__ == '__main__':
+    print("Starting")
+    app.run_server()
+    print("Completed")
